1dec89382bd4615a750f071be9efb2ed120ed048
elisha1995 <elishasamuel01@gmail.com>
2024-11-01 00:23:49 +0000

    Add routing

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { APIResponseModel, IDesignation } from '../../model/interface/role';
+import { MasterService } from './../../services/master.service';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-designation',
   standalone: true,
   imports: [],
   templateUrl: './designation.component.html',
-  styleUrl: './designation.component.css'
+  styleUrl: './designation.component.css',
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
+  designationList: IDesignation[] = [];
+  masterService = inject(MasterService);
 
+  ngOnInit(): void {
+    this.masterService.getDesignation().subscribe(
+      (res: APIResponseModel) => {
+        this.designationList = res.data;
+      },
+      (error) => {
+        alert('API error/ Network Down');
+      }
+    );
+  }
 }
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IRole } from '../../model/interface/role';
+import { APIResponseModel, IRole } from '../../model/interface/role';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -21,9 +21,9 @@ export class RolesComponent implements OnInit {
   }
 
   getAllRoles() {
-    this.http.get(
+    this.http.get<APIResponseModel>(
       'https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles'
-    ).subscribe((res:any)=>{
+    ).subscribe((res:APIResponseModel)=>{
       this.roleList = res.data;
     });
   }
